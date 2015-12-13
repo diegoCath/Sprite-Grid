9513d303a8d310c6c97a3dcfa7c2dc86a1a1372a
Diego Cathalifaud <diego@megatome.local>
2015-12-13 12:07:23 -0300

    Initial Commit

---
+++ Level EditorTests/Level_EditorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Level_EditorTests.swift
+//  Level EditorTests
+//
+//  Created by Diego Cathalifaud on 12/13/15.
+//  Copyright © 2015 Diego Cathalifaud. All rights reserved.
+//
+
+import XCTest
+@testable import Level_Editor
+
+class Level_EditorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
